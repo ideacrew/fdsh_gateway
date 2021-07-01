ac342466fca3a12a07ff7ac4425413973fa35069
Trey Evans <lewis.r.evans@gmail.com>
2021-07-01 16:28:16 -0400

    First service registered.

--- config/initializers/event_source.rb
+++ config/initializers/event_source.rb
@@ -5,7 +5,7 @@ EventSource.configure do |config|
   config.pub_sub_root =
     Pathname.pwd.join('spec', 'rails_app', 'app', 'event_source')
 
-  config.server_key = ENV['RAILS_ENV']
+  config.server_key = ENV['RAILS_ENV'] || Rails.env.to_sym
 
   config.servers do |server|
     server.amqp do |rabbitmq|
@@ -17,6 +17,24 @@ EventSource.configure do |config|
       warn rabbitmq.password
       # rabbitmq.url = "" # ENV['RABBITMQ_URL']
     end
+
+    server.http do |http|
+      http.ref = "http://ridp-service/initial"
+      http.url = ENV["RIDP_INITIAL_SERVICE_URL"] || "http://ridp-service/initial"
+      http.client_certificate do |client_cert|
+        client_cert.client_certificate = ENV["RIDP_CLIENT_CERT_PATH"] || 
+          File.join(File.dirname(__FILE__), "..", "ridp_test_cert.pem")
+        client_cert.client_key = ENV["RIDP_CLIENT_KEY_PATH"] || 
+          File.join(File.dirname(__FILE__), "..", "ridp_test_key.key")
+      end
+      http.soap do |soap|
+        soap.user_name = ENV["RIDP_SERVICE_USERNAME"]
+        soap.password = ENV["RIDP_SERVICE_PASSWORD"]
+        soap.password_encoding = :digest
+        soap.use_timestamp = true
+        soap.timestamp_ttl = 60.seconds
+      end
+    end
   end
 
   app_schemas =
@@ -24,7 +42,7 @@ EventSource.configure do |config|
     .loaded_specs
     .values
     .inject([]) do |ps, s|
-      ps.concat(s.matches_for_glob('aca_entities/async_api/fdsh_gateway.yml'))
+      ps.concat(s.matches_for_glob('aca_entities/async_api/fdsh_gateway/*.yml'))
     end
 
   config.async_api_schemas =
