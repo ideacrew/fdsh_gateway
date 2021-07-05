428612410bc4cbe9103abf14b04348191a5541bc
Dan Thomas <dan@ideacrew.com>
2021-07-05 13:26:01 -0400

    Add RIDP publishers and subscribers and copy over Operations from AcaEntities

--- app/operations/fdsh/ridp/h139/request_primary_determination.rb
+++ app/operations/fdsh/ridp/h139/request_primary_determination.rb
@@ -3,42 +3,85 @@
 require 'dry/monads'
 require 'dry/monads/do'
 
-# High level process flow:
-# 1. Enroll will fire event ridp_eligibility_requested with Family sturctured CV3 payload
-# 2. FDSH Gateway subsribes to these events: Subscribers::RidpElibilityRequestedSubscriber and
-# routes to appropriate operation
-# 3. FDSH Gateway subscribes to Subscribers::Fdsh::Ridp::H139 and calls opertion to parse response
-# 4. Upon success Publish CV3 response
-# 5. Enroll presents questions in form
-# 6. Send question reponse to FDSH for secondary call
-# 7. How do we handle scenrio where customer obtains RIDP using 800 phone number?
+module Fdsh::Operations::Ridp::H139
+  # This class takes AcaEntities::Families::Famlily as input and returns the ridp primary request hash.
+  class RequestPrimaryDetermination
+    include Dry::Monads[:result, :do, :try]
+    include EventSource::Command
 
-module Fdsh
-  module Ridp
-    module H139
-      # Transform CV3 to FDSH Requst and call FDSH RIBP Operation
-      # Use EventSource HTTP SOAP to perform call
-      class PrimaryRequest
-        include Dry::Monads[:result, :do]
-        include EventSource::Command
+    # @param params [Hash] The params to execute an FDSH RIDP Primary Determination Request
+    # @option params [AcaEntities::Families::Family] :family
+    # @return [Dry::Monads::Result]
+    def call(params)
+      family = yield validate_family(params)
+      person = yield fetch_primary_person(family)
+      request = yield build_request(person)
+      validated_request = yield validate_request(request)
+      request_json = yield request_entity_json(validated_request)
+      publish_event(request_json)
 
-        # @param [Hash] params
-        # @option params [AcaEntities::Families::Family] family family entity
-        # @return [Dry::Monad] result
-        def call(params)
-          #          values = yield validate(params)
-          #          fdsh_payload = yield transform(values)
-          #          soap_message = yield build_fdsh_soap_request(fdsh_payload)
+      Success(primary_request_json)
+    end
 
-          # Pattern this after MITC request
-          #          request = yield request_service(soap_message)
-          #          persist_request = yield persist(request)
-          #          event = yield build_event(request)
+    private
 
-          #          Success(request)
-        end
+    def publish_event(request_json)
+      event =
+        event 'organizations.general_organization_created',
+              attributes: organization.to_h
+      event.publish
+      logger.info "Published event: #{event}"
+    end
 
+    # Validate input object
+    def validate_family(family)
+      if family.is_a?(::AcaEntities::Families::Family)
+        Success(family)
+      else
+        Failure(
+          "Invalid Family, given value is not a ::AcaEntities::Families::Family, input_value:#{family}"
+        )
       end
+    end
+
+    def fetch_primary_person(family)
+      primary_family_member =
+        family.family_members.detect(&:is_primary_applicant)
+      if primary_family_member
+        Success(primary_family_member.person)
+      else
+        Failure('No Primary Applicant in family members')
+      end
+    end
+
+    # Transform Person params To PrimaryRequest Contract params
+    def build_request(person)
+      input_hash =
+        person.to_h.merge(
+          {
+            home_address: person.home_address.to_h,
+            home_phone: person.home_phone.to_h
+          }
+        )
+      ::AcaEntities::Fdsh::Transformers::Ridp::PersonToPrimaryRequest.call(
+        input_hash.to_json
+      ) { |record| @transform_result = record }
+      Success(@transform_result)
+    end
+
+    # Validate PrimaryRequest params against PrimaryRequest Contract
+    def validate_primary_request(params)
+      params.merge!({ LevelOfProofingCode: 'LevelThree' })
+      result =
+        ::AcaEntities::Fdsh::Ridp::H139::PrimaryRequestContract.new.call(params)
+
+      result.success? ? Success(result.to_h) : Failure(result)
+    end
+
+    def primary_request_entity_json(values)
+      result = ::AcaEntities::Fdsh::Ridp::H139::PrimaryRequest.new(values.to_h)
+
+      Success(result)
     end
   end
 end
