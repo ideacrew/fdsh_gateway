3bfa36e8e97e67b9457867973203e45fecdb5569
Raghuram Ganjala <raghuramg83@gmail.com>
2021-07-05 19:13:49 -0400

    Fixed subscribers, publisher errors, added event_source initializer

--- app/event_source/publishers/fdsh/eligibilities/hub_connectivity_publisher.rb
+++ app/event_source/publishers/fdsh/eligibilities/hub_connectivity_publisher.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Publishers::Fdsh::Eligibilities
-  class RipdPublisher
+  class HubConnectivityPublisher
     include ::EventSource::Publisher[
               amqp: 'fdsh.eligibilities.hub_connectivity'
             ]
