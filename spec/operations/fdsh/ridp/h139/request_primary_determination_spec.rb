979749ac5c30fa5444bf02bbf1c60080b4f13002
Trey Evans <lewis.r.evans@gmail.com>
2021-07-06 13:37:15 -0400

    Working through failure cases.

--- spec/operations/fdsh/ridp/h139/request_primary_determination_spec.rb
+++ spec/operations/fdsh/ridp/h139/request_primary_determination_spec.rb
@@ -15,3 +15,39 @@ RSpec.describe Fdsh::Ridp::H139::RequestPrimaryDetermination, "given invalid JSO
     expect(subject.failure).to eq :invalid_json
   end
 end
+
+RSpec.describe Fdsh::Ridp::H139::RequestPrimaryDetermination, "given:
+- valid JSON
+- invalid family params" do
+
+  let(:json_payload) { "{}" }
+
+  let(:family_contract_mock) do
+    instance_double(
+      ::AcaEntities::Contracts::Families::FamilyContract
+    )
+  end
+
+  subject do
+    described_class.new.call(json_payload)
+  end
+
+  let(:family_contract_validation_result) do
+    double(
+      success?: false,
+      errors: :invalid_family
+    )
+  end
+
+  before :each do
+    allow(::AcaEntities::Contracts::Families::FamilyContract).to receive(:new)
+      .and_return(family_contract_mock)
+    allow(family_contract_mock).to receive(:call).with({})
+                                                 .and_return(family_contract_validation_result)
+  end
+
+  it "fails" do
+    expect(subject.success?).to be_falsey
+    expect(subject.failure).to eq :invalid_family
+  end
+end
