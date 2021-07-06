dc526d8b8f20869f76ea988d36692772246b51c5
Raghuram Ganjala <raghuramg83@gmail.com>
2021-07-06 14:04:12 -0400

    removed duplicate amqp server configuration

--- config/initializers/event_source.rb
+++ config/initializers/event_source.rb
@@ -8,21 +8,6 @@ EventSource.configure do |config|
 
   config.servers do |server|
     server.amqp do |rabbitmq|
-      rabbitmq.host = ENV['RABBITMQ_HOST'] || 'amqp://localhost'
-      warn rabbitmq.host
-      rabbitmq.vhost = ENV['RABBITMQ_VHOST'] || '/'
-      warn rabbitmq.vhost
-      rabbitmq.port = ENV['RABBITMQ_PORT'] || '5672'
-      warn rabbitmq.port
-      rabbitmq.url = ENV['RABBITMQ_URL'] || 'amqp://localhost:5672/'
-      warn rabbitmq.url
-      rabbitmq.user_name = ENV['RABBITMQ_USERNAME'] || 'guest'
-      warn rabbitmq.user_name
-      rabbitmq.password = ENV['RABBITMQ_PASSWORD'] || 'guest'
-      warn rabbitmq.password
-    end
-
-    server.amqp do |rabbitmq|
       rabbitmq.ref = 'amqp://rabbitmq:5672/event_source'
       rabbitmq.host = ENV['RABBITMQ_HOST'] || 'amqp://localhost'
       warn rabbitmq.host
