0a379ce2215d482754dadc30eacf99aaab42db93
Trey Evans <lewis.r.evans@gmail.com>
2021-07-06 11:26:50 -0400

    Fix rubocop.

--- app/event_source/subscribers/fdsh/determination_requests/ridp_determination_requested_subscriber.rb
+++ app/event_source/subscribers/fdsh/determination_requests/ridp_determination_requested_subscriber.rb
@@ -1,29 +1,33 @@
 # frozen_string_literal: true
 
-module Subscribers::Fdsh::DeterminationRequests
-  # Publish events for FDSH RIDP requests
-  class RidpDeterminationRequestedSubscriber
-    include ::EventSource::Subscriber[amqp: 'fdsh.determination_requests.ridp']
+module Subscribers
+  module Fdsh
+    module DeterminationRequests
+      # Publish events for FDSH RIDP requests
+      class RidpDeterminationRequestedSubscriber
+        include ::EventSource::Subscriber[amqp: 'fdsh.determination_requests.ridp']
 
-    subscribe(
-      :on_fdsh_determination_requests_ridp_primary_determination_requested
-    ) do |_delivery_info, _metadata, payload|
-      # Sequence of steps that are executed as single operation
-      # puts "triggered --> on_primary_request block -- #{delivery_info} --  #{metadata} -- #{payload}"
+        subscribe(
+          :on_fdsh_determination_requests_ridp_primary_determination_requested
+        ) do |_delivery_info, _metadata, payload|
+          # Sequence of steps that are executed as single operation
+          # puts "triggered --> on_primary_request block -- #{delivery_info} --  #{metadata} -- #{payload}"
 
-      if Fdsh::Ridp::H139.BuildPrimaryRequest.call(payload).success?
-        # acknowledge RabbitMQ - will delete message from queue
-      end
-    end
+          if Fdsh::Ridp::H139.BuildPrimaryRequest.call(payload).success?
+            # acknowledge RabbitMQ - will delete message from queue
+          end
+        end
 
-    subscribe(
-      :on_fdsh_determination_requests_ridp_secondary_determination_requested
-    ) do |_delivery_info, _metadata, payload|
-      # Sequence of steps that are executed as single operation
-      # puts "triggered --> on_secondary_request block -- #{delivery_info} --  #{metadata} -- #{payload}"
+        subscribe(
+          :on_fdsh_determination_requests_ridp_secondary_determination_requested
+        ) do |_delivery_info, _metadata, payload|
+          # Sequence of steps that are executed as single operation
+          # puts "triggered --> on_secondary_request block -- #{delivery_info} --  #{metadata} -- #{payload}"
 
-      if Fdsh::Ridp::H139.RequestSecondaryEligibility(payload).success?
-        # acknowledge RabbitMQ - will delete message from queue
+          if Fdsh::Ridp::H139.RequestSecondaryEligibility(payload).success?
+            # acknowledge RabbitMQ - will delete message from queue
+          end
+        end
       end
     end
   end
