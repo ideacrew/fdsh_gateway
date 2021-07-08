a2773478236bb061760fad9674981542f528db58
Trey Evans <lewis.r.evans@gmail.com>
2021-07-08 09:09:19 -0400

    Building out secondary request operations.

--- app/operations/fdsh/ridp/h139/generate_secondary_request_payload.rb
+++ app/operations/fdsh/ridp/h139/generate_secondary_request_payload.rb
@@ -6,13 +6,13 @@ require 'dry/monads/do'
 module Fdsh
   module Ridp
     module H139
-      # This class takes AcaEntities::Families::Famlily as input and returns the ridp secondary request hash.
+      # This class takes AcaEntities::Families::Famlily as input and returns the secondary request.
       class GenerateSecondaryRequestPayload
         include Dry::Monads[:result, :do, :try]
         include AcaEntities::AppHelper
 
         # @param [Hash] opts The options to generate Ridp secondary Request Payload
-        # @return [Dry::Monads::Result]
+        # @return [Dry::Monads::Result<]
         def call(family)
           valid_family                 = yield validate_family(family)
           primary_person               = yield fetch_primary_family_members_person(valid_family)
@@ -52,7 +52,7 @@ module Fdsh
 
         def fetch_secondary_request(evidence)
           if evidence.is_a?(::AcaEntities::Evidences::RidpEvidence)
-            Success(evidence.secondary_request.to_h.to_json)
+            Success(evidence.secondary_request)
           else
             Failure("Invalid Evidence, given value is not a AcaEntities::Evidences::RidpEvidence, input_value:#{evidence}")
           end
