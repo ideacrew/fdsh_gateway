1fa4a4930f4688e058625a15af90ff6277752845
harshaellanki <h_vardhan@yahoo.com>
2021-07-13 12:14:36 -0400

    spec for primary negative response

--- spec/operations/fdsh/ridp/h139/process_primary_response_spec.rb
+++ spec/operations/fdsh/ridp/h139/process_primary_response_spec.rb
@@ -24,3 +24,25 @@ RSpec.describe Fdsh::Ridp::H139::ProcessPrimaryResponse do
     expect(@result.value!).to be_a AcaEntities::Attestations::Attestation
   end
 end
+
+RSpec.describe Fdsh::Ridp::H139::ProcessPrimaryResponse do
+
+  let(:file) do
+    loc = File.join(Rails.root, "spec", "reference", "xml", "ridp", "RIDPPrimaryNegativeResponse.xml")
+    File.expand_path(loc)
+  end
+
+  let(:xml) {Nokogiri::XML(File.open(file))}
+
+  before do
+    @result = described_class.new.call(xml)
+  end
+
+  it "is successful" do
+    expect(@result.success?).to be_truthy
+  end
+
+  it "result to be an Attestation object" do
+    expect(@result.value!).to be_a AcaEntities::Attestations::Attestation
+  end
+end
