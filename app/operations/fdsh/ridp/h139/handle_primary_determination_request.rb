8a61033c6e8cae882f9c232270e746a1bee29004
harshaellanki <h_vardhan@yahoo.com>
2021-07-13 12:37:24 -0400

    added loggers to test

--- app/operations/fdsh/ridp/h139/handle_primary_determination_request.rb
+++ app/operations/fdsh/ridp/h139/handle_primary_determination_request.rb
@@ -11,8 +11,11 @@ module Fdsh
         # @return [Dry::Monads::Result]
         def call(params)
           primary_determination_result_soap = yield RequestPrimaryDetermination.new.call(params[:payload])
+          Rails.logger.info("after primary request #{params} ************************** #{primary_determination_result_soap}")
           primary_determination_result = yield ::Soap::RemoveSoapEnvelope.new.call(primary_determination_result_soap.body)
+          Rails.logger.info("after remove soap envelope  $$$$$$$$$$$$$$$$$$$$$$$$$$ #{primary_determination_result}")
           primary_determination_outcome = yield ProcessPrimaryResponse.new.call(primary_determination_result)
+          Rails.logger.info("after ProcessPrimaryResponse &&&&&&&&&&&&&&&&&&&&&&&&& #{primary_determination_outcome}")
 
           event  = yield build_event(params[:correlation_id], primary_determination_outcome)
           result = yield publish(event)
