7168b6158bfac9b309c19f3f5c3f715f7e6c1430
Dan Thomas <dan@ideacrew.com>
2021-08-12 14:41:13 -0400

    Update to Ruby 2.7.3 & Rails 6.1.4 Add Webpacker

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   options = {
+#     site: 'https://127.0.0.1:3005',
+#     realm: 'me_sbe_bizops'
+#   }
+
+#   provider :keycloak_openid,
+#            ENV['KEYCLOAK_CLIENT_ID'] || 'fdsh_gateway',
+#            ENV['KEYCLOAK_CLIENT_SECRET'] || 'da1a42f5-85fa-4cbd-a972-07abe5bed2ed',
+#            client_options: options
+# end
