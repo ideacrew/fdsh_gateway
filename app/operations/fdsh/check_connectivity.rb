9eddd369c17b88bf28043804f002c699b2c469a4
harshaellanki <h_vardhan@yahoo.com>
2021-08-15 10:57:26 -0400

    esi operations for publish/process

---
+++ app/operations/fdsh/check_connectivity.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+require 'dry/monads/do'
+
+module Fdsh
+  # This class takes happy mapper hash as input and returns
+  class CheckConnectivity
+    include Dry::Monads[:result, :do, :try]
+    include AcaEntities::AppHelper
+
+    PublishEventStruct = Struct.new(:name, :payload, :headers)
+
+    PUBLISH_EVENT = "fdsh_connectivity_check_requested"
+
+    # @param [Hash] opts The options to process
+    # @return [Dry::Monads::Result]
+    def call(_response)
+      publish
+    end
+
+    CONNECTIVITY_TEST_PAYLOAD = <<-XMLCODE
+      <hubc:HubConnectivityRequest xmlns:hubc="http://hubc.ee.sim.dsh.cms.hhs.gov" xmlns:soap="http://www.w3.org/2003/05/soap-envelope">ME</hubc:HubConnectivityRequest>
+    XMLCODE
+
+    private
+
+    def publish
+      event = PublishEventStruct.new(PUBLISH_EVENT, CONNECTIVITY_TEST_PAYLOAD)
+      Success(::Publishers::Fdsh::ConnectivityPublisher.publish(event))
+    end
+  end
+end
