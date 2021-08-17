ce24c72c20dde50a5d76fb8ea3608fe4b0ca9bfb
saipraveen18 <saipraveen.gudimetla@gmail.com>
2021-08-17 17:52:29 -0400

    Fix specs

--- spec/operations/fdsh/esi/h14/process_esi_determination_response_spec.rb
+++ spec/operations/fdsh/esi/h14/process_esi_determination_response_spec.rb
@@ -35,7 +35,7 @@ end
 RSpec.describe Fdsh::Esi::H14::ProcessEsiDeterminationResponse do
 
   let(:file) do
-    loc = File.join(Rails.root, "spec", "reference", "xml", "esi", "TestResponse.xml")
+    loc = File.join(Rails.root, "spec", "reference", "xml", "esi", "MaxResponse.xml")
     File.expand_path(loc)
   end
 
