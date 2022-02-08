355b0c0e27e656a97fd3df59914734825ff2dc7f
Harshavardhan Ellanki <h_vardhan@yahoo.com>
2022-02-08 09:31:12 -0500

    determine rrv status based on the response date (#22)

--- spec/operations/fdsh/rrv/medicare/response/process_rrv_medicare_determination_spec.rb
+++ spec/operations/fdsh/rrv/medicare/response/process_rrv_medicare_determination_spec.rb
@@ -4,101 +4,203 @@ require 'spec_helper'
 require 'medicare_metadata_setup'
 
 RSpec.describe Fdsh::Rrv::Medicare::Response::ProcessRrvMedicareDetermination do
-  context "when application start date is matched with response date" do
-    let(:individual_response_params) do
-      { :PersonSSNIdentification => "011789802",
-        :Insurances => [
-          { :InsuranceEffectiveDate => Date.today }
-        ],
-        :OrganizationResponseCode => "0000",
-        :OrganizationResponseCodeText => "Business Transaction Successful" }
+  describe "when applicant doesn't have medicare beenefits" do
+    context "when application start date is matched with response date" do
+      let(:individual_response_params) do
+        { :PersonSSNIdentification => "011789802",
+          :Insurances => [
+            { :InsuranceEffectiveDate => Date.today }
+          ],
+          :OrganizationResponseCode => "0000",
+          :OrganizationResponseCodeText => "Business Transaction Successful" }
+      end
+
+      let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
+      let(:applicant) {double}
+      let(:application_effective_on) { Date.today }
+
+      before do
+        allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(false)
+      end
+
+      it 'should return outstanding' do
+        result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
+        expect(result).to eq 'outstanding'
+      end
     end
 
-    let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
-    let(:applicant) {double}
-    let(:application_effective_on) { Date.today }
+    context "when application start date is less than response date" do
+      let(:individual_response_params) do
+        { :PersonSSNIdentification => "011789802",
+          :Insurances => [
+            { :InsuranceEffectiveDate => Date.today + 10.days }
+          ],
+          :OrganizationResponseCode => "0000",
+          :OrganizationResponseCodeText => "Business Transaction Successful" }
+      end
 
-    before do
-      allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(false)
+      let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
+      let(:applicant) {double}
+      let(:application_effective_on) { Date.today }
+
+      before do
+        allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(false)
+      end
+
+      it 'should return attested' do
+        result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
+        expect(result).to eq 'attested'
+      end
     end
 
-    it 'should return outstanding' do
-      result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
-      expect(result).to eq 'outstanding'
+    context "when application start date is greater than response start date and less than response end date" do
+      let(:individual_response_params) do
+        { :PersonSSNIdentification => "011789802",
+          :Insurances => [
+            { :InsuranceEffectiveDate => Date.today - 10.days,
+              :InsuranceEndDate => Date.today + 10.days }
+          ],
+          :OrganizationResponseCode => "0000",
+          :OrganizationResponseCodeText => "Business Transaction Successful" }
+      end
+
+      let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
+      let(:applicant) {double}
+      let(:application_effective_on) { Date.today }
+
+      before do
+        allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(false)
+      end
+
+      it 'should return attested' do
+        result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
+        expect(result).to eq 'outstanding'
+      end
+    end
+
+    context "when application start date is greater than response start date and less than response end date" do
+      let(:individual_response_params) do
+        { :PersonSSNIdentification => "011789802",
+          :Insurances => [
+            { :InsuranceEffectiveDate => Date.today - 10.days,
+              :InsuranceEndDate => Date.today - 2.days }
+          ],
+          :OrganizationResponseCode => "0000",
+          :OrganizationResponseCodeText => "Business Transaction Successful" }
+      end
+
+      let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
+      let(:applicant) {double}
+      let(:application_effective_on) { Date.today }
+
+      before do
+        allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(false)
+      end
+
+      it 'should return attested' do
+        result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
+        expect(result).to eq 'attested'
+      end
     end
   end
 
-  context "when application start date is less than response date" do
-    let(:individual_response_params) do
-      { :PersonSSNIdentification => "011789802",
-        :Insurances => [
-          { :InsuranceEffectiveDate => Date.today + 10.days }
-        ],
-        :OrganizationResponseCode => "0000",
-        :OrganizationResponseCodeText => "Business Transaction Successful" }
+  describe "when applicant have medicare beenefits" do
+    context "when application start date is matched with response date" do
+      let(:individual_response_params) do
+        { :PersonSSNIdentification => "011789802",
+          :Insurances => [
+            { :InsuranceEffectiveDate => Date.today }
+          ],
+          :OrganizationResponseCode => "0000",
+          :OrganizationResponseCodeText => "Business Transaction Successful" }
+      end
+
+      let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
+      let(:applicant) {double}
+      let(:application_effective_on) { Date.today }
+
+      before do
+        allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(true)
+      end
+
+      it 'should return outstanding' do
+        result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
+        expect(result).to eq 'attested'
+      end
     end
 
-    let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
-    let(:applicant) {double}
-    let(:application_effective_on) { Date.today }
+    context "when application start date is less than response date" do
+      let(:individual_response_params) do
+        { :PersonSSNIdentification => "011789802",
+          :Insurances => [
+            { :InsuranceEffectiveDate => Date.today + 10.days }
+          ],
+          :OrganizationResponseCode => "0000",
+          :OrganizationResponseCodeText => "Business Transaction Successful" }
+      end
 
-    before do
-      allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(false)
+      let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
+      let(:applicant) {double}
+      let(:application_effective_on) { Date.today }
+
+      before do
+        allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(true)
+      end
+
+      it 'should return attested' do
+        result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
+        expect(result).to eq 'attested'
+      end
     end
 
-    it 'should return attested' do
-      result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
-      expect(result).to eq 'attested'
-    end
-  end
+    context "when application start date is greater than response start date and less than response end date" do
+      let(:individual_response_params) do
+        { :PersonSSNIdentification => "011789802",
+          :Insurances => [
+            { :InsuranceEffectiveDate => Date.today - 10.days,
+              :InsuranceEndDate => Date.today + 10.days }
+          ],
+          :OrganizationResponseCode => "0000",
+          :OrganizationResponseCodeText => "Business Transaction Successful" }
+      end
 
-  context "when application start date is greater than response start date and less than response end date" do
-    let(:individual_response_params) do
-      { :PersonSSNIdentification => "011789802",
-        :Insurances => [
-          { :InsuranceEffectiveDate => Date.today - 10.days,
-            :InsuranceEndDate => Date.today + 10.days }
-        ],
-        :OrganizationResponseCode => "0000",
-        :OrganizationResponseCodeText => "Business Transaction Successful" }
-    end
+      let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
+      let(:applicant) {double}
+      let(:application_effective_on) { Date.today }
 
-    let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
-    let(:applicant) {double}
-    let(:application_effective_on) { Date.today }
+      before do
+        allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(true)
+      end
 
-    before do
-      allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(false)
-    end
-
-    it 'should return attested' do
-      result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
-      expect(result).to eq 'outstanding'
-    end
-  end
-
-  context "when application start date is greater than response start date and less than response end date" do
-    let(:individual_response_params) do
-      { :PersonSSNIdentification => "011789802",
-        :Insurances => [
-          { :InsuranceEffectiveDate => Date.today - 10.days,
-            :InsuranceEndDate => Date.today - 2.days }
-        ],
-        :OrganizationResponseCode => "0000",
-        :OrganizationResponseCodeText => "Business Transaction Successful" }
+      it 'should return attested' do
+        result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
+        expect(result).to eq 'attested'
+      end
     end
 
-    let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
-    let(:applicant) {double}
-    let(:application_effective_on) { Date.today }
+    context "when application start date is greater than response start date and less than response end date" do
+      let(:individual_response_params) do
+        { :PersonSSNIdentification => "011789802",
+          :Insurances => [
+            { :InsuranceEffectiveDate => Date.today - 10.days,
+              :InsuranceEndDate => Date.today - 2.days }
+          ],
+          :OrganizationResponseCode => "0000",
+          :OrganizationResponseCodeText => "Business Transaction Successful" }
+      end
 
-    before do
-      allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(false)
-    end
+      let(:individual_response) { AcaEntities::Fdsh::Rrv::Medicare::IndividualResponse.new(individual_response_params) }
+      let(:applicant) {double}
+      let(:application_effective_on) { Date.today }
 
-    it 'should return attested' do
-      result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
-      expect(result).to eq 'attested'
+      before do
+        allow(applicant).to receive(:health_benefits_for).with('medicare').and_return(true)
+      end
+
+      it 'should return attested' do
+        result = described_class.new.send(:determine_medicare_status, individual_response, applicant, application_effective_on)
+        expect(result).to eq 'attested'
+      end
     end
   end
 end
