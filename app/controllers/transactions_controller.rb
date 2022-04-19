d1c000d3ca92dce339d5cd6e40ac616c488dd0fe
Kristin Merbach <kristin@kristinmerbach.com>
2022-04-19 16:49:23 -0400

    search redo, nav dropdown

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,17 @@
 class TransactionsController < ApplicationController
 
   def index
-    @transactions = Kaminari.paginate_array(Transaction.where(:activities.nin => [nil, []]).map {|t| t.activities.map{|a| {a: a, t: t}}}.flatten).page params[:page]
+  	if params.key?(:search)
+      search_id = params.fetch(:search)
+      @search = search_id unless search_id.blank?
+      @results = Transaction.where(:activities.nin => [nil, []], magi_medicaid_application: /#{search_id}/)
+      redirect_to @results.first if @results&.length == 1
+    end
+    @transactions = if @search
+                      Kaminari.paginate_array(@results.map {|t| t.activities.map{|a| {a: a, t: t}}}.flatten).page params[:page]
+                    else
+                      Kaminari.paginate_array(Transaction.where(:activities.nin => [nil, []]).map {|t| t.activities.map{|a| {a: a, t: t}}}.flatten).page params[:page]
+                    end
   end
 
   def show
