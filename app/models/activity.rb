4ff40504bfe8778d67c6501a78fee288b08a1bad
Kristin Merbach <kristin@kristinmerbach.com>
2022-04-25 06:08:43 -0700

    prettify_xml

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -24,9 +24,55 @@ class Activity
     payload[:application]
   end
 
-  def response_code
-    return "no message" unless message
-    # return  "no response" unless message["response_metadata"]
-    JSON.parse(message.to_json).keys
+  def is_request?
+    return unless message
+    message["request"]
   end
+
+  def is_response?
+    return unless message
+    message["response"]
+  end
+
+  def decrypted_message
+  	return unless message
+  	if is_request?
+  	  pretty_xml(message["request"])
+  	elsif is_response?
+  	  pretty_xml(message["response"])
+    else
+      message
+    end
+  end
+
+  def pretty_xml(xml_text)
+    xml = JSON.parse(decrypt(xml_text))
+    xp(xml)
+  end
+
+  def xp(xml_text)
+    xsl =<<XSL
+    <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:output method="xml" encoding="UTF-8" indent="yes"/>
+    <xsl:strip-space elements="*"/>
+    <xsl:template match="/">
+    <xsl:copy-of select="."/>
+    </xsl:template>
+    </xsl:stylesheet>
+XSL
+
+    doc = Nokogiri::XML(xml_text)
+    xslt = Nokogiri::XSLT(xsl)
+    out = xslt.transform(doc)
+
+    out.to_xml
+  end
+
+  private
+
+  def decrypt(value)
+    AcaEntities::Operations::Encryption::Decrypt.new.call({ value: value }).value!
+  end
+
+
 end
