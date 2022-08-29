6672f6e5e213420f289a7ae649fc9a076277b527
Sarah Austin <scaustin34@gmail.com>
2022-08-29 15:01:41 -0400

    first pass replicating rrv for pvc

---
+++ app/event_source/subscribers/fdsh/pvc_medicaid/individual_responses_subscriber.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Subscribers
+  module Fdsh
+    module PvcMedicaid
+      # Publish events for FDSH SSA requests
+      class IndividualResponsesSubscriber
+        include ::EventSource::Subscriber[amqp: 'fdsh.pvc_medicaid.individual_responses']
+        # rubocop:disable Lint/RescueException
+        subscribe(:on_fdsh_pvc_medicaid_individual_responses) do |delivery_info, _properties, payload|
+          values = JSON.parse(payload, :symbolize_names => true)
+          result = ::Fdsh::Pvc::Medicare::Response::ProcessIndividualPvcResponse.new.call(values[:payload])
+
+          if result.success?
+            logger.info("OK: :on_fdsh_pvc_medicaid_individual_responsess successful and acked")
+          else
+            logger.error("Error: :on_fdsh_pvc_medicaid_individual_responsess; failed due to:#{result.inspect}")
+          end
+          ack(delivery_info.delivery_tag)
+        rescue Exception => e
+          logger.error("Exception: :on_fdsh_pvc_medicaid_individual_responsess\n Exception: #{e.inspect}\n Backtrace:\n #{e.backtrace.join("\n")}")
+          ack(delivery_info.delivery_tag)
+        end
+        # rubocop:enable Lint/RescueException
+      end
+    end
+  end
+end
---
+++ spec/operations/fdsh/pvc/medicare/response/process_pvc_medicare_determination_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+RSpec.describe Fdsh::Pvc::Medicare::Response::ProcessPvcMedicareDetermination, dbclean: :after_each do
+  describe "ProcessPvcMedicareDetermination" do
+    let(:mdcr_payload) do
+      { :IndividualResponses =>
+        [{ :PersonSSNIdentification => "011789802",
+           :Insurances => [{ :InsuranceEffectiveDate => Date.new(2019, 1, 1) }],
+           :OrganizationResponseCode => "0000",
+           :OrganizationResponseCodeText => "Business Transaction Successful" },
+         { :PersonSSNIdentification => "007643003",
+           :Insurances =>
+           [{ :InsuranceEffectiveDate => Date.new(2022, 12, 1),
+              :InsuranceEndDate => Date.new(2022, 12, 31) }],
+           :OrganizationResponseCode => "0000",
+           :OrganizationResponseCodeText => "Business Transaction Successful" }] }
+    end
+
+    let(:medicare_response) { AcaEntities::Fdsh::Pvc::Medicare::EesDshBatchResponseData.new(mdcr_payload) }
+
+    it "should success" do
+      result = described_class.new.call(medicare_response)
+      expect(result).to be_success
+    end
+  end
+end
