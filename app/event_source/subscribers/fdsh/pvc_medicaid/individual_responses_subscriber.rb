0fbb63d2258f28ab24357332c7f9009556fd45e1
mdkaraman <mdkaramanov@gmail.com>
2022-09-13 11:20:50 -0400

    finish event source config

--- app/event_source/subscribers/fdsh/pvc_medicaid/individual_responses_subscriber.rb
+++ app/event_source/subscribers/fdsh/pvc_medicaid/individual_responses_subscriber.rb
@@ -5,20 +5,20 @@ module Subscribers
     module PvcMedicaid
       # Publish events for FDSH SSA requests
       class IndividualResponsesSubscriber
-        include ::EventSource::Subscriber[amqp: 'fdsh.pvc_medicaid.individual_responses']
+        include ::EventSource::Subscriber[amqp: 'fdsh.pvc.individual_responses']
         # rubocop:disable Lint/RescueException
-        subscribe(:on_fdsh_pvc_medicaid_individual_responses) do |delivery_info, _properties, payload|
+        subscribe(:on_fdsh_pvc_individual_responses) do |delivery_info, _properties, payload|
           values = JSON.parse(payload, :symbolize_names => true)
           result = ::Fdsh::Pvc::Medicare::Response::ProcessIndividualPvcResponse.new.call(values[:payload])
 
           if result.success?
-            logger.info("OK: :on_fdsh_pvc_medicaid_individual_responsess successful and acked")
+            logger.info("OK: :on_fdsh_pvc_individual_responsess successful and acked")
           else
-            logger.error("Error: :on_fdsh_pvc_medicaid_individual_responsess; failed due to:#{result.inspect}")
+            logger.error("Error: :on_fdsh_pvc_individual_responsess; failed due to:#{result.inspect}")
           end
           ack(delivery_info.delivery_tag)
         rescue Exception => e
-          logger.error("Exception: :on_fdsh_pvc_medicaid_individual_responsess\n Exception: #{e.inspect}\n Backtrace:\n #{e.backtrace.join("\n")}")
+          logger.error("Exception: :on_fdsh_pvc_individual_responsess\n Exception: #{e.inspect}\n Backtrace:\n #{e.backtrace.join("\n")}")
           ack(delivery_info.delivery_tag)
         end
         # rubocop:enable Lint/RescueException
