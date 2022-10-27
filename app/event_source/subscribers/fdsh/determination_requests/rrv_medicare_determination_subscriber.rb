6ca76dca66bf5af0aba650fb58c3aa51e7ea8a65
Raghuram <raghuramg83@gmail.com>
2022-10-26 21:17:22 -0400

    Added logging for subscriber

--- app/event_source/subscribers/fdsh/determination_requests/rrv_medicare_determination_subscriber.rb
+++ app/event_source/subscribers/fdsh/determination_requests/rrv_medicare_determination_subscriber.rb
@@ -11,17 +11,30 @@ module Subscribers
         # rubocop:disable Style/LineEndConcatenation
         # rubocop:disable Style/StringConcatenation
         subscribe(:on_magi_medicaid_application_renewal_assistance_eligible) do |delivery_info, _properties, payload|
+          subscriber_logger = Logger.new(
+            "#{Rails.root}/log/on_magi_medicaid_application_renewal_assistance_eligible_events_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log"
+          )
+
           # Sequence of steps that are executed as single operation
           values = JSON.parse(payload, :symbolize_names => true)
+
+          subscriber_logger.info "Processing #{values[:applications].count} applications"
           determination_result = ::Fdsh::Rrv::Medicare::CreateRequestManifestFile.new.call(values[:applications])
+          subscriber_logger.info "Processed #{values[:applications].count} applications with result #{determination_result}"
 
           if determination_result.success?
+            subscriber_logger.info "OK: :on_fdsh_rrv_medicare_eligibility_determination_subscriber successful and acked"
             logger.info("OK: :on_fdsh_rrv_medicare_eligibility_determination_subscriber successful and acked")
           else
+            subscriber_logger.info "Error: :on_fdsh_rrv_medicare_eligibility_determination_subscriber; failed due to:#{determination_result.inspect}"
             logger.error("Error: :on_fdsh_rrv_medicare_eligibility_determination_subscriber; failed due to:#{determination_result.inspect}")
           end
           ack(delivery_info.delivery_tag)
         rescue Exception => e
+          subscriber_logger.info(
+            "Exception: :on_fdsh_rrv_medicare_eligibility_determination_subscriber\n Exception: #{e.inspect}" +
+              "\n Backtrace:\n" + e.backtrace.join("\n")
+          )
           logger.error(
             "Exception: :on_fdsh_rrv_medicare_eligibility_determination_subscriber\n Exception: #{e.inspect}" +
               "\n Backtrace:\n" + e.backtrace.join("\n")
