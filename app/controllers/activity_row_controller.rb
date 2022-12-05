a9a1f4e469f1acb017e00b142058c897beb3f735
Sarah Austin <scaustin34@gmail.com>
2022-12-05 15:23:31 -0500

    removed unnecessary sort

--- app/controllers/activity_row_controller.rb
+++ app/controllers/activity_row_controller.rb
@@ -3,18 +3,14 @@
 # ActivityRow controller
 class ActivityRowController < ApplicationController
 
-def index
+  def index
     if params.key?(:search)
       search_value = params.fetch(:search)
       @search = search_value unless search_value.blank?
       @search_results = ActivityRow.or({ primary_hbx_id: search_value }, { application_id: search_value })
     end
-
-    arr = @search ? @search_results : ActivityRow.all
-
-    sorted_arr = arr.sort_by(&:updated_at).reverse!
-
-    @activity_rows = Kaminari.paginate_array(sorted_arr).page params[:page]
+    results = @search ? @search_results : ActivityRow.all
+    @activity_rows = results.page params[:page]
   end
 
 end
