0bc1706a9d74a2a9cd5c3017a8d4ab24d6d17f18
Marco Ornelas <marco.ornelas@ideacrew.com>
2023-01-18 11:54:54 -0500

    cleanup and rubocop changes

--- spec/models/pdm_request_spec.rb
+++ spec/models/pdm_request_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe PdmRequest, type: :model do
@@ -5,11 +7,11 @@ RSpec.describe PdmRequest, type: :model do
     DatabaseCleaner.clean
   end
 
-  let!(:valid_params) {{ :subject_id => "abc", :command => "something" }}
+  let!(:valid_params) { { :subject_id => "abc", :command => "something" } }
 
   context 'valid params' do
     before do
-      @manifest = PdmManifest.new({:assistance_year => 1000, :type => "something" })
+      @manifest = PdmManifest.new({ :assistance_year => 1000, :type => "something" })
       @manifest.save!
 
       @request = described_class.new(valid_params)
@@ -18,14 +20,8 @@ RSpec.describe PdmRequest, type: :model do
     end
 
     it 'should be findable' do
-      # expect(described_class.find(@request)).to be_a(PdmRequest)
-      # obj = @manifest.pdm_requests.first
-      # binding.irb
-      # expect(obj).to be_a(PdmRequest)
       result = @manifest.pdm_requests.find(@request)
       expect(result).to be_a(PdmRequest)
     end
   end
-
-
 end
