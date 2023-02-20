7dc435ba821a5a98c50af70f024da77f58b4a48e
Saipraveen Gudimetla <saipraveen.gudimetla@gmail.com>
2023-02-19 20:26:20 -0500

    add spec for transmission find_or_create

---
+++ spec/operations/fdsh/h36/transmissions/find_or_create_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Fdsh::H36::Transmissions::FindOrCreate do
+  subject { described_class.new.call(input_params) }
+
+  before :all do
+    DatabaseCleaner.clean
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+
+  describe '#call' do
+    context 'with valid input params' do
+      context 'without an open H36 transmission' do
+        let(:input_params) { { assistance_year: Date.today.year } }
+        it 'creates an open H36 transmission' do
+          expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.open.count).to be_zero
+          subject
+          expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.open.count).to eq(1)
+        end
+      end
+
+      context 'with an open H36 transmission with same reporting year' do
+        let(:input_params) { { assistance_year: Date.today.year } }
+        let!(:month_of_year_transmission) do
+          FactoryBot.create(:month_of_year_transmission, reporting_year: Date.today.year)
+        end
+
+        it 'returns the existing open H36 transmission' do
+          expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.open.count).to eq(1)
+          subject
+          expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.open.count).to eq(1)
+        end
+      end
+    end
+
+    context 'with invalid input params' do
+      context 'bad input' do
+        let(:input_params) { {} }
+
+        it 'returns failure with errors' do
+          expect(subject.failure).to eq('Please pass in assistance_year')
+        end
+      end
+    end
+  end
+end
