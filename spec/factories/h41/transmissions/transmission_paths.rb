b7acc16a95f7259bf2165510412c4acb90fa3d13
Sai Kumar Koptagiri <saikumarkotagiri9@gmail.com>
2023-02-23 10:49:04 -0500

    fetch transmission_path and fix build_xml for no_transmit blocked voids

--- spec/factories/h41/transmissions/transmission_paths.rb
+++ spec/factories/h41/transmissions/transmission_paths.rb
@@ -3,5 +3,9 @@
 FactoryBot.define do
   factory :transmission_path, class: "::H41::Transmissions::TransmissionPath" do
     association :transaction, factory: :transmittable_transaction
+
+    batch_reference        { '2023-02-10T21:43:39Z' }
+    content_file_id        { '00001' }
+    record_sequence_number { '223456000000001' }
   end
 end
