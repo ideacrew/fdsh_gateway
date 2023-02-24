6a2f003f49f55c1175461ef9412f0c3b274b22ce
Battula <vrbattula@outlook.com>
2023-02-24 15:42:04 -0500

    add notice events for void, corrected and initial

---
+++ app/event_source/events/fdsh_gateway/irs1095as/corrected_notice_requested.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Events
+  module FdshGateway
+    module Irs1095as
+      # This class will register event
+      class VoidNoticeRequested < EventSource::Event
+        publisher_path 'publishers.fdsh_gateway.irs1095as_publisher'
+
+      end
+    end
+  end
+end
