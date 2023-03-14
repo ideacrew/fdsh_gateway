84c735ed9598bb481ed515951e057488e736df74
Saipraveen Gudimetla <saipraveen.gudimetla@gmail.com>
2023-03-14 12:25:59 -0400

    fix publisher and subscriber for h36 build xml

--- app/event_source/events/fdsh/h36/build_xml_requested.rb
+++ app/event_source/events/fdsh/h36/build_xml_requested.rb
@@ -4,8 +4,8 @@ module Events
   module Fdsh
     module H36
       # This class will register event
-      class BuildH36XmlRequested < EventSource::Event
-        publisher_path 'publishers.fdsh.h36.build_h36_xml_requested_publisher'
+      class BuildXmlRequested < EventSource::Event
+        publisher_path 'publishers.fdsh.h36.build_xml_requested_publisher'
       end
     end
   end
