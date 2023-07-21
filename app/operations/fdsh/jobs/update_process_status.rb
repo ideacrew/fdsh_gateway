c11fa7899c676eeba1217cd57cfe711f0c1b6ef3
Kristin Merbach <kristin@kristinmerbach.com>
2023-07-21 10:44:11 -0700

    set ended_at for the last process state for completed statuses

--- app/operations/fdsh/jobs/update_process_status.rb
+++ app/operations/fdsh/jobs/update_process_status.rb
@@ -30,7 +30,11 @@ module Fdsh
                                                                                                 message: values[:message],
                                                                                                 started_at: DateTime.now,
                                                                                                 state_key: values[:state])
-          transmittable_object.ended_at = DateTime.now if [:failed, :completed, :succeeded].include?(values[:state])
+          if [:failed, :completed, :succeeded].include?(values[:state])
+            transmittable_object.ended_at = DateTime.now
+            last_process_state = transmittable_object&.process_status&.process_states&.last
+            last_process_state.ended_at = DateTime.now if last_process_state
+          end
           transmittable_object.save
         end
         Success("Process status updated successfully")
