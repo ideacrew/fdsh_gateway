4fa9da2def891de06b23a7fbcefb479b9abd61ac
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2023-09-13 11:11:11 -0400

    add rescue and check if response is json before trying to parse (#155)
    * add rescue and check if response is json before trying to parse
    
    * rubocop fixes
    
    * oauth spec
    
    * fix failures
    
    * removed whitespace
    
    ---------
    
    Co-authored-by: haridhar yamjala <hariym471@gmail.com>

--- app/operations/jwt/get_jwt.rb
+++ app/operations/jwt/get_jwt.rb
@@ -18,8 +18,9 @@ module Jwt
       path = ENV['TOKEN_PATH'] || 'auth/oauth/v2/token'
       token_response = Rails.cache.fetch("cms_access_token", expires_in: 29.minutes.to_i, race_condition_ttl: 5.seconds) do
         auth_conn = Faraday.new(url: host)
-        response = auth_conn.post(path, grant_type: "client_credentials", client_id: ENV['TOKEN_CLIENT_ID'],
-                                        client_secret: ENV['TOKEN_CLIENT_SECRET'])
+        response = auth_conn.post(path, grant_type: "client_credentials", client_id: ENV.fetch('TOKEN_CLIENT_ID', nil),
+                                        client_secret: ENV.fetch('TOKEN_CLIENT_SECRET', nil))
+        return Failure("Non JSON response for JWT request") unless json?(response.env.response_body)
         resp = JSON.parse(response.env.response_body, symbolize_names: true)
 
         break if resp[:errors]
@@ -27,6 +28,14 @@ module Jwt
         resp[:access_token]
       end
       token_response ? Success(token_response) : Failure("Unable to fetch JWT")
+    rescue StandardError => e
+      Failure("Error while fetching JWT: #{e.message}")
+    end
+
+    def json?(response)
+      !JSON.parse(response).nil?
+    rescue StandardError
+      false
     end
 
   end
--- spec/operations/fdsh/check_oauth_connectivity_spec.rb
+++ spec/operations/fdsh/check_oauth_connectivity_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Fdsh::CheckOauthConnectivity, "given invalid JSON" do
   end
 
   context "valid token" do
-
     before(:each) do
       stub_request(:post, "https://impl.hub.cms.gov/auth/oauth/v2/token")
         .with(
@@ -46,7 +45,6 @@ RSpec.describe Fdsh::CheckOauthConnectivity, "given invalid JSON" do
   end
 
   context "invalid token" do
-
     before(:each) do
       stub_request(:post, "https://impl.hub.cms.gov/auth/oauth/v2/token")
         .with(
@@ -65,6 +63,48 @@ RSpec.describe Fdsh::CheckOauthConnectivity, "given invalid JSON" do
       expect(subject.success?).to be_falsey
       expect(subject.failure).to be_truthy
     end
+  end
+
+  context 'xml response' do
+
+    before(:each) do
+      stub_const('ENV', 'TOKEN_HOST' => 'https://impl.hub.cms.gov/HubConnectivityServiceRest')
+      stub_request(:post, "https://impl.hub.cms.gov/HubConnectivityServiceRest/auth/oauth/v2/token")
+        .with(
+          body: { "client_id" => nil, "client_secret" => nil, "grant_type" => "client_credentials" },
+          headers: {
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'User-Agent' => 'Faraday v1.4.3'
+          }
+        )
+        .to_return(status: 500, body: '<?xml version="1.0" encoding="UTF-8"?>
+      <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+          <soapenv:Body>
+              <soapenv:Fault>
+                  <faultcode>soapenv:Server</faultcode>
+                  <faultstring>Policy Falsified</faultstring>
+                  <faultactor>https://impl.hub.cms.gov/Imp1/VerifySSACompositeService</faultactor>
+                  <detail>
+                      <l7:policyResult
+                          status="Service Not Found.  The request may have been sent to an invalid URL"
+                          xmlns:l7="http://www.layer7tech.com/ws/policy/fault"/>
+                  </detail>
+              </soapenv:Fault>
+          </soapenv:Body>
+      </soapenv:Envelope>', headers: {})
+    end
+    it "should fail" do
+
+      expect(subject.success?).to be_falsey
+      expect(subject.failure).to be_truthy
+    end
+
+    it "should return the failure message" do
+      expect(subject.failure).to eq "Non JSON response for JWT request"
+    end
+
   end
 end
 
