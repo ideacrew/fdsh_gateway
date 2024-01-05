4fa9aa341737d48da1b9d303ceee35d7e8e96647
vishal kalletla <vishuk1201@gmail.com>
2024-01-05 17:06:43 -0500

    specs and build fix 1_4_2024 (#180)
    * fix transform_family_payload spec
    
    * fix update_h36_transactions spec
    
    * rubocop fix
    
    * set rubygems-update version to 3.4.22
    
    * rubocop fix

--- spec/operations/fdsh/h36/irs_groups/update_h36_transactions_spec.rb
+++ spec/operations/fdsh/h36/irs_groups/update_h36_transactions_spec.rb
@@ -43,32 +43,90 @@ RSpec.describe Fdsh::H36::IrsGroups::UpdateH36Transactions do
     end
   end
 
-  context "valid params" do
-    let(:irs_group) do
-      create(:h36_irs_group, assistance_year: Date.today.year - 1)
+  context "when current date is between January and March" do
+    before :each do
+      allow(Date).to receive(:today).and_return(Date.new(2024, 1, 1))
     end
 
-    let(:transmission) do
-      create(:month_of_year_transmission, reporting_year: Date.today.year, status: :open,
-                                          month_of_year: Date.today.month - 1)
+    context "valid params" do
+      let(:irs_group) do
+        create(:h36_irs_group, assistance_year: Date.today.year - 1)
+      end
+
+      let!(:transmission_previous_year) do
+        create(:month_of_year_transmission, reporting_year: Date.today.year - 1, status: :open,
+                                            month_of_year: 12 + Date.today.month)
+      end
+
+      let!(:transmission_current_year) do
+        create(:month_of_year_transmission, reporting_year: Date.today.year, status: :open,
+                                            month_of_year: Date.today.month == 1 ? Date.today.month : Date.today.month - 1)
+      end
+
+      let!(:transaction_current_year) do
+        create_list(:transmittable_transaction, 5, transmit_action: :transmit, status: :created,
+                                                   started_at: Time.now, transactable: irs_group,
+                                                   transmission: transmission_current_year)
+      end
+
+      let!(:transaction_previous_year) do
+        create_list(:transmittable_transaction, 5, transmit_action: :transmit, status: :created,
+                                                   started_at: Time.now, transactable: irs_group,
+                                                   transmission: transmission_previous_year)
+      end
+
+      it "should create a new open transmission and should not update prior transmission to pending state" do
+        expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.count).to eq 2
+        current_month = Date.today.month == 1 ? Date.today.month + 1 : Date.today.month - 1
+        result = Fdsh::H36::IrsGroups::UpdateH36Transactions.new.call(assistance_year: Date.today.year, current_month: current_month)
+        expect(result.success?).to be_truthy
+        transmission_previous_year.reload
+        transmission_current_year.reload
+        expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.count).to eq 4
+        expect(transmission_previous_year.status).to eq :pending
+        expect(transmission_current_year.status).to eq :pending
+        expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.last.status).to eq :open
+        expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.last.transactions.count).to eq 5
+      end
+    end
+  end
+
+  context "when current date is between April and December" do
+    before :each do
+      allow(Date).to receive(:today).and_return(Date.new(2023, 12, 1))
     end
 
-    let!(:transaction) do
-      create_list(:transmittable_transaction, 5, transmit_action: :transmit, status: :created,
-                                                 started_at: Time.now, transactable: irs_group,
-                                                 transmission: transmission)
-    end
+    context "valid params" do
+      let(:irs_group) do
+        create(:h36_irs_group, assistance_year: Date.today.year - 1)
+      end
 
-    it "should create a new open transmission and should not update prior transmission to pending state" do
-      expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.count).to eq 1
-      result = Fdsh::H36::IrsGroups::UpdateH36Transactions.new.call(assistance_year: Date.today.year,
-                                                                    current_month: Date.today.month)
-      expect(result.success?).to be_truthy
-      transmission.reload
-      expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.count).to eq 2
-      expect(transmission.status).to eq :pending
-      expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.last.status).to eq :open
-      expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.last.transactions.count).to eq 5
+      let(:transmission) do
+        create(:month_of_year_transmission, reporting_year: Date.today.year, status: :open,
+                                            month_of_year: Date.today.month - 1)
+      end
+
+      let!(:transaction) do
+        create_list(:transmittable_transaction, 5, transmit_action: :transmit, status: :created,
+                                                   started_at: Time.now, transactable: irs_group,
+                                                   transmission: transmission)
+      end
+
+      it "should create a new open transmission and should not update prior transmission to pending state" do
+        expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.count).to eq 1
+        result = Fdsh::H36::IrsGroups::UpdateH36Transactions.new.call(assistance_year: Date.today.year,
+                                                                      current_month: Date.today.month)
+        expect(result.success?).to be_truthy
+        transmission.reload
+        expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.count).to eq 2
+        expect(transmission.status).to eq :pending
+        expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.last.status).to eq :open
+        expect(::H36::Transmissions::Outbound::MonthOfYearTransmission.all.last.transactions.count).to eq 5
+      end
     end
+  end
+
+  after do
+    allow(Date).to receive(:today).and_return(Date.today)
   end
 end
