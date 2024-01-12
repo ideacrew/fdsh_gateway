6d5390dbd0ca3258ef8c33ab0ee4bbb919d30537
TristanB17 <tristan0bambauer@gmail.com>
2024-01-12 13:59:26 -0700

    change comment to read more accurately

--- app/event_source/subscribers/fdsh/vlp/verify_vlp_service_rest_subscriber.rb
+++ app/event_source/subscribers/fdsh/vlp/verify_vlp_service_rest_subscriber.rb
@@ -2,18 +2,18 @@
 
 module Subscribers
   module Fdsh
-    # Receive response from cms requests for ssa json payload
+    # Receive response from cms requests for VLP json payload
     class VerifyVlpCompositeServiceRestSubscriber
       include ::EventSource::Subscriber[http: '/VerifyLawfulPresenceServiceV37.1Rest']
 
       subscribe(:'on_VerifyLawfulPresenceServiceV37.1Rest') do |body, status, _headers|
         if status.to_s == "200"
-          logger.info "Subscribers::Fdsh::SsaServiceSubscribe: on_VerifyVLPServiceRest OK #{status}, #{body}"
+          logger.info "Subscribers::Fdsh::VLPServiceSubscribe: on_VerifyVLPServiceRest OK #{status}, #{body}"
         else
-          logger.error "Subscribers::Fdsh::SsaServiceSubscribe: on_VerifyVLPServiceRest error #{status}, #{body}"
+          logger.error "Subscribers::Fdsh::VLPServiceSubscribe: on_VerifyVLPServiceRest error #{status}, #{body}"
         end
       rescue StandardError => e
-        logger.error "Subscribers::Fdsh::SsaServiceSubscribe: on_VerifyVLPServiceRest error backtrace: #{e.inspect}, #{e.backtrace}"
+        logger.error "Subscribers::Fdsh::VLPServiceSubscribe: on_VerifyVLPServiceRest error backtrace: #{e.inspect}, #{e.backtrace}"
       end
     end
   end
