b6a3c015b41a3aa2925da702f74d2ccb4a037479
Michael Karamanov <michael.karamanov@ideacrew.com>
2024-01-24 13:38:10 -0500

    remove sign up option from devise config and app layout (#193)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   ## Database authenticatable
