ff7d1b8e380d9290e3bce2b0c616791201334932
Jacob Kagon <jacob@kagon.net>
2024-02-09 17:53:16 -0500

    update devise routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@
 
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :skip => [:registrations] 
+    as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    end
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end  
 
   root 'activity_row#index'
 
