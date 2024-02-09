3fb307026731473ac296ccdfe5f1c464eb7843c4
Jacob Kagon <jacob@kagon.net>
2024-02-09 17:56:18 -0500

    rubocop fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@
 
 Rails.application.routes.draw do
 
-  devise_for :users, :skip => [:registrations] 
-    as :user do
+  devise_for :users, :skip => [:registrations]
+  as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
-    end
+  end
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
-  end  
+  end
 
   root 'activity_row#index'
 
