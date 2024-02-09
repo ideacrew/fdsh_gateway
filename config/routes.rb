2bf2dfa6dbc349ee4b6f70d29c3f3c46e532af8d
Jacob Kagon <jacob@kagon.net>
2024-02-09 18:08:06 -0500

    refactor devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@
 
 Rails.application.routes.draw do
 
-  devise_for :users, :skip => [:registrations]
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
-
+  devise_for :users, skip: [:registrations]
   devise_scope :user do
+    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', as: 'user_registration'
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
