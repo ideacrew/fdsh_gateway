a6870120e2805cfe2da1b2edd4361ae0b23897d8
Jacob Kagon <jacob@kagon.net>
2024-02-12 09:07:55 -0500

    allow sessions destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
     put 'users' => 'devise/registrations#update', as: 'user_registration'
-    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   root 'activity_row#index'
