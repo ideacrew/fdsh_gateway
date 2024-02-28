957bbd4d5c839ee90664256d7b297f39278290b6
vkghub <vishuk1201@gmail.com>
2024-02-28 14:44:29 -0500

    update rrv filename structure based on cms eft serverless feature

--- app/operations/fdsh/rrv/medicare/request/create_batch_request_file.rb
+++ app/operations/fdsh/rrv/medicare/request/create_batch_request_file.rb
@@ -35,9 +35,24 @@ module Fdsh
             Fdsh::Rrv::Medicare::Request::CreateManifestFile.new.call(values)
           end
 
+          # Generates the name for the zip folder based on the current timestamp and the feature flag `:cms_eft_serverless`.
+          #
+          # If the feature `:cms_eft_serverless` is enabled in `FdshGatewayRegistry`, the folder name is formatted as:
+          # /SBE00ME.DSH.RRVIN.DYYMMDD.THHMMSSMMM.P.zip
+          # Otherwise, the folder name is formatted as:
+          # /SBE00ME.DSH.RRVIN.DYYMMDD.THHMMSSMMM.P.IN.zip
+          #
+          # @param outbound_folder [String] The base folder where the zip file will be created.
+          # @return [String] The full path of the zip file.
+          def fetch_zip_folder_name(outbound_folder)
+            base_format = "/SBE00ME.DSH.RRVIN.D#{Time.now.strftime('%y%m%d.T%H%M%S%L.P')}"
+            extension = FdshGatewayRegistry.feature_enabled?(:cms_eft_serverless) ? ".zip" : ".IN.zip"
+            "#{outbound_folder}#{base_format}#{extension}"
+          end
+
           def generate_batch_zip(values, manifest_file)
             input_files = [File.basename(values[:transaction_file]), File.basename(manifest_file)]
-            @zip_name = values[:outbound_folder] + "/SBE00ME.DSH.RRVIN.D#{Time.now.strftime('%y%m%d.T%H%M%S%L.P')}.IN.zip"
+            @zip_name = fetch_zip_folder_name(values[:outbound_folder])
 
             Zip::File.open(@zip_name, create: true) do |zipfile|
               input_files.each do |filename|
